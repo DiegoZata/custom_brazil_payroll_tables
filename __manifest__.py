cf5af36bf593427d5b7924707e5657bbd4ab2d62
DiegoZata <dgbv_@hotmail.com>
2025-01-03 09:43:32 -0300

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,21 @@
 {
     'name': 'Custom Brazil Payroll Tables',
     'version': '1.0',
-    'author': 'Diego Benitz',
+    'author': 'Diego Benitez',
     'category': 'Human Resources',
     'summary': 'Adds customizable tables for payroll calculations (INSS, IRRF, FGTS) in Brazil.',
-    'depends': ['hr_payroll'],
+    'depends': [
+        'hr_payroll',
+        'hr_contract',
+    ],
     'data': [
         'security/ir.model.access.csv',
         'views/payroll_tables_views.xml',
         'views/payroll_menuitems.xml',
+        'views/contract_view.xml',
         'data/payroll_default_data.xml',
     ],
     'installable': True,
     'application': False,
 }
+
